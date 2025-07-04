333b0d4eedfeec05f7a558f68bb1c2a263029431
Vineet Seth <vs4622@srmist.edu.in>
2025-07-05 00:25:57 +0530

    Update Max_min_sum.py

--- TUPLE/Max_min_sum.py
+++ TUPLE/Max_min_sum.py
@@ -1,13 +1,13 @@
 # Input
-elements = tuple(map(int, input("Enter numbers separated by spaces: ").split()))
+Elements = tuple(map(int, input("Enter numbers separated by spaces: ").split()))
 
 # Operations
-max_value = max(elements)
-min_value = min(elements)
-total_sum = sum(elements)
+max_value = max(Elements)
+min_value = min(Elements)
+total_sum = sum(Elements)
 
 # Output
-print(f"Tuple: {elements}")
+print(f"Tuple: {Elements}")
 print(f"Maximum value: {max_value}")
 print(f"Minimum value: {min_value}")
 print(f"Sum of elements: {total_sum}")
