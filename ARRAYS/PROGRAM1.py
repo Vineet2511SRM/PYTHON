3a82c77b7903f4476df6be7223608c9bb8d5e2d7
Vineet Seth <vs4622@srmist.edu.in>
2025-07-06 04:48:02 +0530

    Update PROGRAM1.py

--- ARRAYS/PROGRAM1.py
+++ ARRAYS/PROGRAM1.py
@@ -2,10 +2,10 @@
 array = list(map(int, input("Enter numbers separated by spaces: ").split()))
 
 # Finding maximum and minimum values
-max_value = max(array)
-min_value = min(array)
+maximum_value = max(array)
+minimum_value = min(array)
 
 # Output
 print(f"Array: {array}")
-print(f"Maximum value: {max_value}")
-print(f"Minimum value: {min_value}")
+print(f"Maximum value: {maximum_value}")
+print(f"Minimum value: {minimum_value}")
