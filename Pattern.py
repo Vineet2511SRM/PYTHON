229555ab8cd43c6f2cc1e4c75dcc8f9acda7bbeb
Vineet2511SRM <vs4622@srmist.edu.in>
2025-08-13 11:02:52 +0530

    Pattern

--- Pattern.py
+++ Pattern.py
@@ -1,16 +1,19 @@
 from turtle import *
-from colorsys import*
+from colorsys import hsv_to_rgb
+
 tracer(50)
 bgcolor("black")
 pensize(3)
-h=0
+
+h = 0
 for i in range(350):
-    h+=0.0021
-    c=hsv_to_rgb(1,h,1)
+    h += 0.0021
+    c = hsv_to_rgb(h % 1, 1, 1)  # Cycle hue smoothly
     color(c)
-    goto(0,0)
-    circle(180-i/4,90)
+    goto(0, 0)
+    circle(180 - i/4, 90)
     circle(40)
-    circle(180-i/4,-60)
+    circle(180 - i/4, -60)
     right(50)
-done
+
+done()
